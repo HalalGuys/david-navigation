98b7c9aede73860ac1f1c00a2db9af43d3b24365
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-08-02 18:23:41 -0700

    Setup service

---
+++ client/index.jsx
@@ -0,0 +1,3 @@
+import App from './components/App.jsx';
+
+Window.App = App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',      
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
+        use: ['file-loader']  
+      }
+    ]
+  }
+};
